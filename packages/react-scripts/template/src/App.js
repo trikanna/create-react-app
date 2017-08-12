8d77cd1314b9ff29263490efb353a43a28d18b3d
Santiago Camelo <trikanna@gmail.com>
2017-08-12 03:19:56 -0300

    minimal setup

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background: #f2f2f2;
+  text-align: center;
+  padding: 0 15px;
+
+  &:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+`;
+
+const Header = styled.header`
+  border-bottom: 1px solid #fefefe;
+  padding-bottom: 10px;
+  margin-bottom: 14px;
+
+  .logo {
+    height: 150px;
+    margin: 0 auto;
+  }
+`;
+
+const Intro = styled.p`
+  max-width: 600px;
+  margin: 0 auto 13pt;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Wrapper>
+        <Header>
+          <img src={logo} className="logo" alt="logo" />
+          <h1>Welcome to React</h1>
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+          <h3>Welcome to React</h3>
+          <h4>Welcome to React</h4>
+          <h5>Welcome to React</h5>
+          <h6>Welcome to React</h6>
+        </Header>
+        <Intro>To get started, edit <code>src/App.js</code> and save to reload. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt accusamus laudantium rerum, repudiandae, qui velit nulla consectetur, doloremque, perferendis inventore cupiditate! Ex iusto optio, praesentium consequatur laboriosam odio atque itaque.</Intro>
+      </Wrapper>
     );
   }
 }
